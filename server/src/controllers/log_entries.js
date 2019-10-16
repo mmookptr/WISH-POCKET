cc2b126b03f015c110558820bd2185089c077728
sidtipat.k <s.kietchai@outlook.com>
2019-10-16 17:19:07 +0700

    finished rest api using node,express,mondodb

--- server/src/controllers/log_entries.js
+++ server/src/controllers/log_entries.js
@@ -1,31 +1,44 @@
-'use strict';
+"use strict";
 
-require('../models/log_entry ')
+require("../models/log_entry ");
 
-exports.index = function(req,res){
-    LogEntry.find({}, function(err,LogEntry){
-        if (err) res.send(err)
-        res.json(LogEntry)
-    })
-}
+exports.index = function(req, res) {
+  LogEntry.find({}, function(err, LogEntry) {
+    if (err) res.send(err);
+    res.json(LogEntry);
+  });
+};
 
 exports.create = function(req, res) {
-    const newLogEntry = new LogEntry(req.body)
-    newLogEntry.save(function(err, LogEntry){
-        if (err) res.send(err)
-        res.json(LogEntry)
-    })
-}
+  const newLogEntry = new LogEntry(req.body);
+  newLogEntry.save(function(err, LogEntry) {
+    if (err) res.send(err);
+    res.json(LogEntry);
+  });
+};
 
-exports.show = function(req,res) {
-    LogEntry.findById(req.params.id,function(err,LogEntry){
-        if (err) res.send(err)
-        res.json(LogEntry)
-    })
-}
+exports.show = function(req, res) {
+  LogEntry.findById(req.params.id, function(err, LogEntry) {
+    if (err) res.send(err);
+    res.json(LogEntry);
+  });
+};
 
-exports.update = function(req,res){
-    LogEntry.findOneAndUpdate({_id: req.params.id},req.body,{new: true},function(err,LogEntry){
-        
-    })
-}
+exports.update = function(req, res) {
+  LogEntry.findOneAndUpdate(
+    { _id: req.params.id },
+    req.body,
+    { new: true },
+    function(err, logEntry) {
+      if (err) res.send(err);
+      res.json(logEntry);
+    }
+  );
+};
+
+exports.destroy = function(req, res) {
+  LogEntry.deleteOne({ _id: req.params.id }, function(err, logEntry) {
+    if (err) res.send(err);
+    res.json({ messae: "Log entry (${req.params.id})}) was delete" });
+  });
+};
