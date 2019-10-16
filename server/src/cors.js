fa4276bbf60bbeaed4d458856559f5ec1b610496
sidtipat.k <s.kietchai@outlook.com>
2019-10-16 23:42:33 +0700

    add comment

--- server/src/cors.js
+++ server/src/cors.js
@@ -1,17 +1,18 @@
-'use strict';
+"use strict";
 
 module.exports = function(app) {
-  const cors = require('cors')
+  const cors = require("cors");
   const corsOptions = {
     origin: function(origin, callback) {
-      if (process.env.CORS_WHITELIST.split(' ').indexOf(origin) !== -1) {
-        callback(null, true)
+      if (process.env.CORS_WHITELIST.split(" ").indexOf(origin) !== -1) {
+        callback(null, true);
+        //check is not undifine or pro
       } else {
-        callback(new Error('Not allowed by CORS configuration'))
+        callback(new Error("Not allowed by CORS configuration"));
       }
     },
-    methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-  }
-  app.options('*', cors(corsOptions))
-  app.use(cors(corsOptions))
-}
+    methods: "GET, HEAD, PUT, PATCH, POST, DELETE"
+  };
+  app.options("*", cors(corsOptions));
+  app.use(cors(corsOptions));
+};
