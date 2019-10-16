1aa12e6fd1b037a6f81ae438e0d2b4bbb910cf58
sidtipat.k <s.kietchai@outlook.com>
2019-10-16 15:27:23 +0700

    finish server confiq

---
+++ server/src/index.js
@@ -0,0 +1,26 @@
+// use for booting up the server
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+// line between my file and library file
+const routes = require("./routes");
+const cors = require("./cors");
+//databse stuff
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/wishpocket-server', { useNewUrlParser:true })
+mongoose.set('useFindAndModify',false)
+
+app.use(bodyParse.urlencoded({ extended: true }))
+app.use(bodyParser.json() )
+
+// cors(app)
+// routes(app)
+app.listen(port)
+console.log('WI$H POCKET server is running at port '+port)
+
+
+
