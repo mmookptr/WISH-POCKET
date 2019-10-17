39cad8ebbe5b1b5666656bbd7267f8dc845be75a
sidtipat.k <s.kietchai@outlook.com>
2019-10-17 18:08:10 +0700

    add dotenv instruction for developer,add test script

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,16 @@ import express from "express";
 
 const app: express.Application = express();
 
+// If you do not have .env please uncomment these 2 lines
+// const host = "localhost"
+// const port = 3000
+
+// and comment these 2 lines
 const host = process.env.HOST as string;
 const port = parseInt(process.env.PORT as string);
 
 app.get("/", (request, response) => {
-  response.send("Hello");
+  response.send("Helloo");
 });
 
 app.listen(port, host, () => {
